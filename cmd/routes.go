9ce9a968048424fad75a231607e04986553cbefd
Erfei Yu <105307186+454270186@users.noreply.github.com>
2024-02-19 15:54:34 +0800

    feat: add cors middleware

--- cmd/routes.go
+++ cmd/routes.go
@@ -3,12 +3,16 @@ package main
 import (
 	"api.backend.xjco2913/controller/user"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
 
 	userController := user.NewUserController()
+
+	// global middleware
+	r.Use(cors.Default())
 
 	api := r.Group("/api")
 	{
