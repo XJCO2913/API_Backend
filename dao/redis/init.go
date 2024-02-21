6426f78d4e47544e58476195e17c734b1f7908cc
Deck-Wang416 <sc212yw@leeds.ac.uk>
2024-02-21 13:34:22 +0800

    feat: finished test of redis api.

--- dao/redis/init.go
+++ dao/redis/init.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"strconv"
-	"sync"
 
 	"api.backend.xjco2913/util/config"
 	"github.com/go-redis/redis/v8"
@@ -12,38 +11,35 @@ import (
 
 var (
 	rdb  *redis.Client
-	once sync.Once
 )
 
-func InitRedis() {
-	once.Do(func() {
-		// Read Redis information from configuration
-		host := config.Get("database.redis.host")
-		port := config.Get("database.redis.port")
-		password := config.Get("database.redis.password")
-		dbStr := config.Get("database.redis.db")
+func init() {
+	// Read Redis information from configuration
+	host := config.Get("database.redis.host")
+	port := config.Get("database.redis.port")
+	password := config.Get("database.redis.password")
+	dbStr := config.Get("database.redis.db")
 
-		db, err := strconv.Atoi(dbStr)
-        if err != nil {
-            panic(fmt.Sprintf("Invalid Redis DB number: %s", dbStr))
-        }
+	db, err := strconv.Atoi(dbStr)
+	if err != nil {
+		panic(fmt.Sprintf("Invalid Redis DB number: %s", dbStr))
+	}
 
-		// Construct Redis connection string
-		addr := fmt.Sprintf("%s:%s", host, port)
+	// Construct Redis connection string
+	addr := fmt.Sprintf("%s:%s", host, port)
 
-		// Create Redis client
-		rdb = redis.NewClient(&redis.Options{
-			Addr:     addr,
-			Password: password, 
-			DB:       db,
-		})
-
-		// test connection
-		ctx := context.Background()
-		if _, err := rdb.Ping(ctx).Result(); err != nil {
-			panic("unable to connect to Redis: " + err.Error())
-		}
+	// Create Redis client
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password, 
+		DB:       db,
 	})
+
+	// test connection
+	ctx := context.Background()
+	if _, err := rdb.Ping(ctx).Result(); err != nil {
+		panic("unable to connect to Redis: " + err.Error())
+	}
 }
 
 // RDB returns a singleton of the Redis client
