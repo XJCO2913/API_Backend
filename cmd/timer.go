6b5a1be871c34042aa59cfe80f00e04f8be0e59c
Erfei Yu <105307186+454270186@users.noreply.github.com>
2024-03-13 17:56:25 +0800

    feat: async flush logs from redis to mysql

---
+++ cmd/timer.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"api.backend.xjco2913/dao/redis"
+)
+
+func FlushLogs(ctx context.Context) {
+	ticker := time.NewTicker(5 * time.Second)
+	for range ticker.C {
+		redis.SyncLogs(ctx, LOG_KEY)
+		redis.SyncLogs(ctx, ERR_LOG_KEY)
+		redis.SyncLogs(ctx, SQL_LOG_KEY)
+	}
+}
