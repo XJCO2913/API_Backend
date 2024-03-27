c7704ef66f4de2ff8d26592fa65478f69744065c
Deck-Wang416 <sc212yw@leeds.ac.uk>
2024-03-27 17:45:53 +0800

    feat: encapsulated GenerateJWTToken func in util.

--- util/util.go
+++ util/util.go
@@ -1,44 +1,67 @@
 package util
 
-import "reflect"
-
 import (
-    "golang.org/x/crypto/bcrypt"
+	"reflect"
+
+	"api.backend.xjco2913/service/sdto/errorx"
+	"api.backend.xjco2913/util/config"
+	"api.backend.xjco2913/util/zlog"
+	"github.com/golang-jwt/jwt/v5"
+	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func IsEmpty(value interface{}) bool {
-    if value == nil {
-        return true
-    }
+	if value == nil {
+		return true
+	}
 
-    v := reflect.ValueOf(value)
-    switch v.Kind() {
-    case reflect.String, reflect.Array, reflect.Map, reflect.Slice:
-        return v.Len() == 0
-    case reflect.Bool:
-        return !v.Bool()
-    case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-        return v.Int() == 0
-    case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-        return v.Uint() == 0
-    case reflect.Float32, reflect.Float64:
-        return v.Float() == 0
-    case reflect.Interface, reflect.Ptr:
-        return v.IsNil()
-    }
+	v := reflect.ValueOf(value)
+	switch v.Kind() {
+	case reflect.String, reflect.Array, reflect.Map, reflect.Slice:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Ptr:
+		return v.IsNil()
+	}
 
-    return false
+	return false
 }
 
 func EncryptPassword(password string) (string, error) {
-    hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-    if err != nil {
-        return "", err
-    }
-    return string(hash), nil
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
 }
 
 func VerifyPassword(hash string, password string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func GenerateJWTToken(claims jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	secret := config.Get("jwt.secret")
+	if IsEmpty(secret) {
+		zlog.Error("jwt.secret is empty in config")
+		return "", errorx.NewInternalErr()
+	}
+
+	tokenStr, err := token.SignedString([]byte(secret))
+	if err != nil {
+		zlog.Error("Error while signing jwt", zap.Error(err))
+		return "", errorx.NewInternalErr()
+	}
+
+	return tokenStr, nil
 }
