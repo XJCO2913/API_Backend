5dc8c3395da9045a75ff25e0da40cd1558e56b14
Erfei Yu <105307186+454270186@users.noreply.github.com>
2024-03-30 16:25:06 +0800

    to: minio set-up for user avartar

--- dao/minio/utils.go
+++ dao/minio/utils.go
@@ -1,8 +1,12 @@
 package minio
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
+	"net/url"
+	"time"
 
 	"github.com/minio/minio-go/v7"
 )
@@ -31,4 +35,39 @@ func CreateBucket(bucketName string) error {
 	}
 
 	return nil
+}
+
+func UploadUserAvatar(ctx context.Context, avatarName string, avatarData []byte) error {
+	avatarReader := bytes.NewReader(avatarData)
+
+	err := UploadFile(ctx, AVATAR_BUCKET, avatarName, avatarReader, int64(len(avatarData)), "image/jpeg")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UploadFile(ctx context.Context, bucketName, objectName string, reader io.Reader, objectSize int64, contentType string) error {
+	_, err := minioClient.PutObject(ctx, bucketName, objectName, reader, objectSize, minio.PutObjectOptions{
+		ContentType: contentType,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetObjectUrl(ctx context.Context, bucketName, objectName string, exp time.Duration) (*url.URL, error) {
+	if exp < 1 {
+		exp = 24 * time.Hour
+	}
+
+	preSignedUrl, err := minioClient.PresignedGetObject(ctx, bucketName, objectName, exp, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return preSignedUrl, nil
 }
