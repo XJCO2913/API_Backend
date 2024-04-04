602a662876e56ba156b057a1e53ee415a5ee69f4
Erfei Yu <105307186+454270186@users.noreply.github.com>
2024-04-04 12:52:14 +0800

    fix: remove coverFile in dto

--- controller/dto/activity_dto.go
+++ controller/dto/activity_dto.go
@@ -1,12 +1,9 @@
 package dto
 
-import "mime/multipart"
-
 type CreateActivityReq struct {
 	Name        string                `form:"name" binding:"required"`
 	Description *string               `form:"description" binding:"required"`
 	RouteID     int32                 `form:"routeId"`
-	CoverFile   *multipart.FileHeader `form:"coverFile" binding:"required"`
 	StartDate   string                `form:"startDate" binding:"required"`
 	EndDate     string                `form:"endDate" binding:"required"`
 	Tags        string                `form:"tags"`
