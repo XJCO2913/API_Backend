50f38220e51fb791a52a9bb46f17e1b634428d02
Deck-Wang416 <sc212yw@leeds.ac.uk>
2024-04-13 18:48:32 +0800

    to: added GetActivitiesByUserID in dao.

--- dao/activity_user_dao.go
+++ dao/activity_user_dao.go
@@ -15,3 +15,29 @@ func CreateActivityUser(ctx context.Context, newUserActivity *model.ActivityUser
 
 	return nil
 }
+
+func GetActivitiesByUserID(ctx context.Context, userID string) ([]*model.Activity, error) {
+	var activityUsers []*model.ActivityUser
+	var activities []*model.Activity
+
+	a := query.Use(DB).ActivityUser
+	activityUsers, err := a.WithContext(ctx).Where(a.UserID.Eq(userID)).Find()
+	if err != nil {
+		return nil, err
+	}
+
+	activityIDs := make([]string, len(activityUsers))
+	for i, userActivity := range activityUsers {
+		activityIDs[i] = userActivity.ActivityID
+	}
+
+	if len(activityIDs) > 0 {
+		a := query.Use(DB).Activity
+		activities, err = a.WithContext(ctx).Where(a.ActivityID.In(activityIDs...)).Find()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return activities, nil
+}
