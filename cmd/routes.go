f62657a73200eafbe838e2b032bf009298903b31
Erfei Yu <105307186+454270186@users.noreply.github.com>
2024-04-25 15:54:24 +0800

    fix: remove local cors setting

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"net/http"
 	"time"
 
 	"api.backend.xjco2913/controller/activity"
@@ -29,19 +28,6 @@ func NewRouter() *gin.Engine {
 
 	// Global middleware
 	// Prometheus
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-
-		c.Next()
-	})
 	r.Use(middleware.PrometheusRequests())
 	r.Use(middleware.PrometheusDuration())
 	r.Use(middleware.PrometheusResErr())
