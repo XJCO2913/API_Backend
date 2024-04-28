640d474edcc1de6bf30dc21675b2d3805aaa47c5
Deck-Wang416 <sc212yw@leeds.ac.uk>
2024-04-29 02:32:18 +0800

    chore: modified sdto.

--- service/sdto/like_service.go
+++ service/sdto/like_service.go
@@ -1,6 +1,6 @@
 package sdto
 
 type CreateLikeInput struct {
-	UserID   string `json:"userID" binding:"required"`
-	MomentID string `json:"momentID" binding:"required"`
+	UserID   string
+	MomentID string
 }
