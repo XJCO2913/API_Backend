57f297fa7eefeb7ce0965ac871dd560a3f13d2a9
Deck-Wang416 <sc212yw@leeds.ac.uk>
2024-04-30 00:15:36 +0800

    fix: added check for moment existence in comment service.

--- service/comment/comment_service.go
+++ service/comment/comment_service.go
@@ -2,6 +2,7 @@ package comment
 
 import (
 	"context"
+	"errors"
 
 	"api.backend.xjco2913/dao"
 	"api.backend.xjco2913/dao/model"
@@ -9,6 +10,7 @@ import (
 	"api.backend.xjco2913/service/sdto/errorx"
 	"api.backend.xjco2913/util/zlog"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 type CommentService struct{}
@@ -22,7 +24,18 @@ func Service() *CommentService {
 }
 
 func (s *CommentService) Create(ctx context.Context, input *sdto.CreateCommentInput) *errorx.ServiceErr {
-	err := dao.CreateNewComment(ctx, &model.Comment{
+	_, err := dao.GetMomentByID(ctx, input.MomentID)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			zlog.Warn("Moment not found by moment ID", zap.String("momentID", input.MomentID))
+			return errorx.NewServicerErr(errorx.ErrExternal, "Moment not found by moment ID", nil)
+		} else {
+			zlog.Error("Failed to retrieve moment by moment ID", zap.String("momentID", input.MomentID), zap.Error(err))
+			return errorx.NewInternalErr()
+		}
+	}
+
+	err = dao.CreateNewComment(ctx, &model.Comment{
 		AuthorID: input.AuthorID,
 		MomentID: input.MomentID,
 		Content:  input.Content,
